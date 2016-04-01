4c297cd083b6c7d517997999a2106ec44b08c45c
Matthew Zwick <moxie@Matthews-MacBook-Pro.local>
2016-04-01 14:09:29 -0400

    First Commit

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,17 @@
+class VotesController < ApplicationController
+
+  def create
+    @link = Link.find(params[:link_id])
+    @link.votes.create
+    redirect_to root_url
+  end
+
+  def downvote
+    @link = Link.find(params[:link_id])
+    @link.votes.create
+    redirect_to root_url
+  end
+
+
+
+end
